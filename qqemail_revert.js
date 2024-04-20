2020f6869d39faa9309cbb50ede8361fe3eb4cb8
lisir <1612886895@qq.com>
2024-04-20 12:05:07 +0800

    qqemail_revert

--- qqemail_revert.js
+++ qqemail_revert.js
@@ -6,19 +6,34 @@
 //     await new Promise(resolve => setTimeout(resolve, 1000));
 // }
 
-// fetch('https://raw.githubusercontent.com/lIIIIlwd/public/master/qqemail_revert.js')
+// ======================================== 调用脚本 ==============================================================
+// 假设你的脚本URL支持查询字符串参数  
+// const baseUrl = 'https://raw.githubusercontent.com/lIIIIlwd/public/master/qqemail_revert.js'
+// const params = {
+//     t: "2023-10-27 12:00:00"
+// };
+// const scriptUrl = addQueryParams(url, params);
+
+// fetch(scriptUrl)
 //     .then(response => response.text())
-//     .then(text => {
-//         eval(text);
+//     .then(scriptText => {
+//         const script = document.createElement('script');
+//         script.textContent = scriptText;
+//         document.body.appendChild(script);
+//         // script.remove();
 //     })
 //     .catch(error => console.error('Error fetching script:', error));
 
 
-// 使用示例  
+
+// ======================================== 脚本执行 ==============================================================
+console.log(window.myParam);
+return;
+// 开始时间  
 const simplifiedDateString = "2023-10-27 12:00:00";
+// 开始时间戳
 const secondsTimestamp = simplifiedDateToSecondsTimestamp(simplifiedDateString);
 console.log(simplifiedDateString); 	// 输出秒级时间戳
-// console.log(secondsTimestamp); 		// 输出秒级时间戳
 
 
 // 使用示例：从当前时间戳开始，到1小时后的时间戳结束，每10秒发送一个请求  
@@ -27,6 +42,22 @@ const endTimestamp = startTimestamp + 3600 * 6; // 1小时后
 const intervalInSeconds = 10; // 每10秒  
 
 traverseTimestamps(startTimestamp, endTimestamp, intervalInSeconds);
+
+function addQueryParams(url, params) {
+    // 将参数对象转换为查询字符串  
+    const queryParams = Object.keys(params)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+        .join('&');
+
+    // 检查URL是否已经有查询参数  
+    if (url.includes('?')) {
+        // 如果已经有参数，用&连接新参数  
+        return `${url}&${queryParams}`;
+    } else {
+        // 如果没有参数，添加?和参数  
+        return `${url}?${queryParams}`;
+    }
+}
 
 function simplifiedDateToSecondsTimestamp(dateString) {
     // 根据简化的日期时间格式构建Date对象  
