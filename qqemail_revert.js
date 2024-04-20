e1e9f758061778f6e3704f0bc91e5fae62cbec2b
lisir <1612886895@qq.com>
2024-04-20 11:37:35 +0800

    qqemail_revert

--- qqemail_revert.js
+++ qqemail_revert.js
@@ -1,7 +1,14 @@
-for(i=0;i<3600*5;i+10){
-	QMAjax.send("/cgi-bin/mail_mgr",{method:"POST",content:["sid=",getSid(),"&mailaction=mail_revert&t=mail_mgr2&timekey=",(1698465600+i),"&logtype=2"].join('')});
-	QMAjax.send("/cgi-bin/do",{method:"POST",content:["sid=",getSid(),"&action=checkpoint&url=https%3A%2F%2Fmail.qq.com%2Fcgi-bin%2Fmail_list%3Fsid%3D0r3MM4Cw9Nb0UjJU%26folderid%3D1%26folderkey%3D1%26page%3D0%26s%3Dinbox%26topmails%3D0%26showinboxtop%3D1%26ver%3D333481.2%26cachemod%3Dmaillist%26cacheage%3D7200%26r%3D1713523618231"].join('')});
+for (i = 0; i < 3600 * 5; i + 10) {
+    QMAjax.send("/cgi-bin/mail_mgr", { method: "POST", content: ["sid=", getSid(), "&mailaction=mail_revert&t=mail_mgr2&timekey=", (1698465600 + i), "&logtype=2"].join('') });
+    QMAjax.send("/cgi-bin/do", { method: "POST", content: ["sid=", getSid(), "&action=checkpoint&url=https%3A%2F%2Fmail.qq.com%2Fcgi-bin%2Fmail_list%3Fsid%3D0r3MM4Cw9Nb0UjJU%26folderid%3D1%26folderkey%3D1%26page%3D0%26s%3Dinbox%26topmails%3D0%26showinboxtop%3D1%26ver%3D333481.2%26cachemod%3Dmaillist%26cacheage%3D7200%26r%3D1713523618231"].join('') });
 
-	console.log('Taking a break...');  
-	await new Promise(resolve => setTimeout(resolve, 1000));
+    console.log('Taking a break...');
+    // await new Promise(resolve => setTimeout(resolve, 1000));
 }
+
+// fetch('https://raw.githubusercontent.com/lIIIIlwd/public/master/qqemail_revert.js')
+//     .then(response => response.text())
+//     .then(text => {
+//         eval(text);
+//     })
+//     .catch(error => console.error('Error fetching script:', error));
